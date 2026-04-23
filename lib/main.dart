0ba6832c9627325b169bcfa943033fd45ef7b15a
Stephen Curry <97381466+warriors-dev23@users.noreply.github.com>
2026-04-23 15:29:47 +0700

    Develop (#11)
    * setup firebase
    
    * ui app eventgo
    
    * screen_event_booking_70%
    
    * payment zalopay
    
    * done code auth
    
    * done home_format_code
    
    * done
    
    * done search logic
    
    * done_profile_screen
    
    * format code app string vs app size
    
    * format appSizes
    
    * add check-in
    
    * format code string
    
    * Logic change language
    
    * Logic search  with category and filter
    
    * Format change language
    
    * done doan
    
    * good luck
    
    * good luck
    
    * Refactor code (#9)
    
    * fix color harcode
    
    * refactor code
    
    * Refactor code (#10)
    
    * fix color harcode
    
    * refactor code
    
    * add readme.md
    
    * resolve confict
    
    * add readme.md
    
    ---------
    
    Co-authored-by: LÊ VĂN NHẬT <97381466+levannhat123@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,113 @@
+import 'dart:async';
+
+import 'package:event_go/core/constants/app_colors.dart';
+import 'package:event_go/core/config/supabase_config.dart';
+import 'package:event_go/firebase_options.dart';
+import 'package:event_go/injection/injection.dart';
+import 'package:event_go/presentation/view_models/auth_change_notifier.dart';
+import 'package:event_go/presentation/view_models/auth_view_model.dart';
+import 'package:event_go/presentation/view_models/locale_langue/locale_language_view_model.dart';
+import 'package:event_go/routers/app_route.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:provider/provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'core/utils/lang/config/app_locale_language.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'core/utils/lang/language.dart';
 
 void main() {
-  runApp(const MyApp());
+  runZonedGuarded<Future<void>>(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await initializeDateFormatting('vi_VN', null);
+      await Hive.initFlutter();
+      await Hive.openBox('settings');
+      await dotenv.load(fileName: '.env');
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+
+      await Supabase.initialize(
+        url: SupabaseConfig.url,
+        anonKey: SupabaseConfig.anonKey,
+      );
+      final appRouter = AppRouter(AuthChangeNotifier());
+
+      SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(
+          statusBarColor: AppColors.background,
+          statusBarIconBrightness: Brightness.dark,
+        ),
+      );
+
+      setupDependencies(appRouter.router);
+
+      runApp(
+        MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (_) => AuthChangeNotifier()),
+            ChangeNotifierProvider(create: (_) => getIt<AuthViewModel>()),
+            ChangeNotifierProvider.value(value: getIt<LocaleNotifier>()),
+          ],
+          child: ScreenUtilInit(
+            designSize: const Size(428, 926),
+            minTextAdapt: true,
+            splitScreenMode: true,
+            builder: (context, child) {
+              return MyApp(router: appRouter.router);
+            },
+          ),
+        ),
+      );
+    },
+    (error, stack) {
+      debugPrint('Error: $error');
+      debugPrint('Stack: $stack');
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final GoRouter router;
 
-  // This widget is the root of your application.
+  const MyApp({super.key, required this.router});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      supportedLocales: Language.all,
+      locale: context.watch<LocaleNotifier>().locale,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        scaffoldBackgroundColor: AppColors.background,
+        fontFamily: 'IBMPlexSans',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.background,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: AppColors.textPrimary),
+          bodyMedium: TextStyle(color: AppColors.textPrimary),
+          bodySmall: TextStyle(color: AppColors.textPrimary),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
     );
   }
 }
